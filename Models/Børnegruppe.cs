1d69767577a24903b8c4447476a77336230aa93e
imeugenecarlo <beugenecarlo@gmail.com>
2023-11-20 10:48:23 +0100

    Updated Børnegruppe.cs med values

--- Models/Børnegruppe.cs
+++ Models/Børnegruppe.cs
@@ -1,6 +1,19 @@
-﻿namespace LodSalgsSystemFDF.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace LodSalgsSystemFDF.Models
 {
-    public class Børnegruppe
+    [Table("Børnegruppe")]
+    public partial class Børnegruppe
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Gruppenavn { get; set; }
+        
+        public string Lokale { get; set; }
+        public int Antalbørn { get; set; }
+        public int LederId { get; set; }
     }
 }

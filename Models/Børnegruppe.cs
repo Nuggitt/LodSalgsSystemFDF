1607bdcb4bea935ef8a036ab7aa0aa0bf26a6309
imeugenecarlo <beugenecarlo@gmail.com>
2023-11-20 11:48:54 +0100

    updated Børnegruppe.cs med Required tag

--- Models/Børnegruppe.cs
+++ Models/Børnegruppe.cs
@@ -7,11 +7,11 @@ namespace LodSalgsSystemFDF.Models
     public partial class Børnegruppe
     {
         [Key]
-        public int Id { get; set; }
+        public int Børnegruppe_Id { get; set; }
         [Required]
         [StringLength(30)]
         public string Gruppenavn { get; set; }
-        
+        [Required]
         public string Lokale { get; set; }
         public int Antalbørn { get; set; }
         public int LederId { get; set; }

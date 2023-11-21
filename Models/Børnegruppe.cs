ff42dcf964613eb2abad7b1ac3bff175b77ca2a2
imeugenecarlo <beugenecarlo@gmail.com>
2023-11-21 16:57:05 +0100

    small fix

--- Models/Børnegruppe.cs
+++ Models/Børnegruppe.cs
@@ -7,13 +7,13 @@ namespace LodSalgsSystemFDF.Models
     public partial class Børnegruppe
     {
         [Key]
-        public int Børnegruppe_Id { get; set; }
+        public int Børnegruppe_ID { get; set; }
         [Required]
         [StringLength(30)]
         public string Gruppenavn { get; set; }
         [Required]
         public string Lokale { get; set; }
         public int Antalbørn { get; set; }
-        public int LederId { get; set; }
+        public int Leder_ID { get; set; }
     }
 }

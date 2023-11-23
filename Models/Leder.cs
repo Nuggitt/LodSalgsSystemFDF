420e81ddbfb6a07d7b22296f70a6f44d71bf80fc
Glenning <glenning.thor@gmail.com>
2023-11-23 11:55:41 +0100

    Created CRUD for Leder

--- Models/Leder.cs
+++ Models/Leder.cs
@@ -1,6 +1,27 @@
-﻿namespace LodSalgsSystemFDF.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace LodSalgsSystemFDF.Models
 {
-    public class Leder
-    {
+        [Table("Leder")]
+        public partial class Leder
+        {
+            [Key]
+            public int Leder_ID { get; set; }
+            [Required]
+            [StringLength(30)]
+            public string Navn { get; set; }
+            [Required]
+            public string Email { get; set; }
+            [Required]
+            public string Adresse { get; set; }
+            [Required]
+            public string Telefon {  get; set; }
+            [Required]
+            public bool ErLotteriBestyrer { get; set; }
+            [Required]
+            public int Ark_ID { get; set; }
+            [Required]
+            public int Børnegruppe_ID { get; set; }
     }
 }
--- Services/ADOServices/ADOLederService/AdonetLederService.cs
+++ Services/ADOServices/ADOLederService/AdonetLederService.cs
@@ -1,6 +1,49 @@
-﻿namespace LodSalgsSystemFDF.Services.ADOServices.ADOLederService
+﻿using LodSalgsSystemFDF.Models;
+using System.Data.SqlClient;
+
+namespace LodSalgsSystemFDF.Services.ADOServices.ADOLederService
 {
     public class AdonetLederService
     {
+        private IConfiguration configuration { get; }
+        string connectionString;
+
+        public AdonetLederService() { }
+
+        public AdonetLederService(IConfiguration config)
+        {
+            configuration = config;
+            connectionString = configuration.GetConnectionString("Datacraft.dk");
+        }
+        public List<Leder> GetAllLeder()
+        {
+            List<Leder> lederList = new List<Leder>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string sql = "SELECT * FROM Leder";
+                SqlCommand command = new SqlCommand(sql, connection);
+                using (SqlDataReader dataReader = command.ExecuteReader())
+                {
+                    while (dataReader.Read())
+                    {
+                        Leder leder = new Leder();
+                        leder.Leder_ID = Convert.ToInt32(dataReader["Leder_ID"]);
+                        leder.Navn = Convert.ToString(dataReader["Navn"]);
+                        leder.Adresse = Convert.ToString(dataReader["Adresse"]);
+                        leder.Telefon = Convert.ToString(dataReader["Telefon"]);
+                        leder.Email = Convert.ToString(dataReader["Email"]);
+                        leder.ErLotteriBestyrer = Convert.ToBoolean(dataReader["ErLotteriBestyrer"]);
+                        leder.Ark_ID = Convert.ToInt32(dataReader["Ark_ID"]);
+                        leder.Børnegruppe_ID = Convert.ToInt32(dataReader["Børnegruppe_ID"]);
+                        
+                        lederList.Add(leder);
+
+
+                    }
+                }
+            }
+            return lederList;
+        }
     }
 }
